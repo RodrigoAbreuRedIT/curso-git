10b6ff4e9beffc062a02efdaf821c7b6f4a307b8
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-11-25 13:15:06 +0000

    Adicionar separadores

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -3,10 +3,11 @@
 namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("-----------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom Dia");
-
+            Console.WriteLine("-----------------------------------");
             Console.ReadLine();
         }
     }
