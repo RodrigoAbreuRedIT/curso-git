4577c86d5a148919751c599945007fb6d2bd5cb9
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-11-25 11:00:16 +0000

    Acrescentada mensagem de bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -4,6 +4,9 @@ namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia");
+
+            Console.ReadLine();
         }
     }
 }
