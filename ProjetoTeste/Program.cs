608c79437e4cda543f4eaf250bce32ee42ff1156
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-11-25 11:21:12 +0000

    acrescentada a pergunta como voces esta hoje

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -5,6 +5,7 @@ namespace ProjetoTeste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom Dia");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
